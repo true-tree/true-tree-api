872e4ae0ad91a941530fe417762aecea785f2863
pursue503 <pursue503@naver.com>
2022-06-17 23:08:23 +0900

    first commit

---
+++ src/main/java/com/truetree/TruetreeApplication.java
@@ -0,0 +1,13 @@
+package com.truetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruetreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruetreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/truetree/TruetreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruetreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
