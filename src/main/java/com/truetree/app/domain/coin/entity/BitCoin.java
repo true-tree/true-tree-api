95beac4630ed6815de2d1df91f2816741859994a
pursue503 <pursue503@naver.com>
2022-06-19 19:15:49 +0900

    Add Default Setting And Test Data Get API

---
+++ src/main/java/com/truetree/app/domain/coin/entity/BitCoin.java
@@ -0,0 +1,52 @@
+package com.truetree.app.domain.coin.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+/**
+ *
+ * 임시용 비코 엔티티
+ *
+ */
+
+@Getter
+@NoArgsConstructor
+@Table(name = "btcusdt")
+@Entity
+public class BitCoin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime openTime;
+
+    private long open;
+    private long high;
+    private long close;
+    private long volume;
+    private long quoteAv;
+    private long trades;
+    private long takerBuyVolume;
+    private long takerBuyBaseAssetVolume;
+    private String symbol;
+
+    @Builder
+    public BitCoin(LocalDateTime openTime, long open, long high, long close, long volume, long quoteAv, long trades, long takerBuyVolume, long takerBuyBaseAssetVolume, String symbol) {
+        this.openTime = openTime;
+        this.open = open;
+        this.high = high;
+        this.close = close;
+        this.volume = volume;
+        this.quoteAv = quoteAv;
+        this.trades = trades;
+        this.takerBuyVolume = takerBuyVolume;
+        this.takerBuyBaseAssetVolume = takerBuyBaseAssetVolume;
+        this.symbol = symbol;
+    }
+}
--- src/main/java/com/truetree/config/security/SecurityConfig.java
+++ src/main/java/com/truetree/config/security/SecurityConfig.java
@@ -2,17 +2,34 @@ package com.truetree.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
+/**
+ *
+ * Spring Security Config
+ * Lambda DSL 사용
+ *
+ * @author pursue503
+ */
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                );
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorizeRequest ->
+                        authorizeRequest.antMatchers("/**").permitAll()
+                )
+                .headers(headers -> headers.frameOptions().disable())
+                .formLogin()
+                .disable()
+                .logout(LogoutConfigurer::permitAll);
+
         return http.build();
     }
 
