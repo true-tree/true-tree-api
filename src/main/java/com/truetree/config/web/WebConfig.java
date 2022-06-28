4e19cb5ae5056215debe2d028bfd4be91800601b
pursue503 <pursue503@naver.com>
2022-06-29 01:11:21 +0900

    Update WebConfig : @Configuration 어노테이션 추가

--- src/main/java/com/truetree/config/web/WebConfig.java
+++ src/main/java/com/truetree/config/web/WebConfig.java
@@ -1,10 +1,12 @@
 package com.truetree.config.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     /**
