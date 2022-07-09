049a12134380334c8f1e32db445c0d59bb50b6d0
la1av1a <zldqkrktqk@gmail.com>
2022-07-10 05:28:40 +0900

    Style: exist 사용, 메소드명 카멜케이스로 변경

--- src/main/java/com/truetree/app/domain/main/game/entity/GameRepository.java
+++ src/main/java/com/truetree/app/domain/main/game/entity/GameRepository.java
@@ -2,12 +2,13 @@ package com.truetree.app.domain.main.game.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 
 public interface GameRepository extends JpaRepository<Game,Long> {
 
-    @Query(value = "select distinct g from Game g join fetch g.memberId where g.memberId.id = :memberId")
-    Optional<Game> findByMemberId_Id(Long memberId);
+    @Query("SELECT COUNT (g.id) >0 " +
+            "FROM Game g " +
+            "WHERE g.member.id = :memberId")
+    boolean existByMemberId(Long memberId);
 
 }
