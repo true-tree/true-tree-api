58625e58ee2c349fdb03267c9edd85379eb30bdd
la1av1a <zldqkrktqk@gmail.com>
2022-07-12 01:34:45 +0900

    Style: 불필요한 if문 삭제, Controller에 있던 비즈니스 로직 이동

--- src/main/java/com/truetree/app/domain/main/game/service/GameService.java
+++ src/main/java/com/truetree/app/domain/main/game/service/GameService.java
@@ -1,9 +1,11 @@
 package com.truetree.app.domain.main.game.service;
 
+import com.truetree.app.domain.coin.service.BitCoinService;
 import com.truetree.app.domain.main.game.dto.GameResponseDTO;
 import com.truetree.app.domain.main.game.entity.Game;
 import com.truetree.app.domain.main.game.entity.GameRepository;
 import com.truetree.app.domain.main.member.entity.Member;
+import com.truetree.app.domain.main.member.service.oauth.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,20 +17,24 @@ import java.util.Optional;
 public class GameService {
 
     private final GameRepository gameRepository;
+    private final MemberService memberService;
+    private final BitCoinService bitCoinService;
 
     @Transactional(readOnly = true)
     public boolean isGameExist(Long memberId){
 
-        if(gameRepository.existByMemberId(memberId))
-            return true;
+        return gameRepository.existsByMemberId(memberId);
 
-        return false;
     }
 
     @Transactional
-    public void createGame(Member member,long gameDataAmount){
+    public void createGame(Long memberId){
+        long gameDataAmount = bitCoinService.getAmount();
 
-        Integer startNumber = Math.toIntExact((long) (Math.random() * (gameDataAmount - 500)) + 101);
+        //101 <= startNumber <= (데이터의 총 개수-499)
+        Integer startNumber = Math.toIntExact((long) (Math.random() * (gameDataAmount - 499)) + 101);
+
+        Member member = memberService.getMemberData(memberId);
 
         gameRepository.save(
                 Game.builder()
