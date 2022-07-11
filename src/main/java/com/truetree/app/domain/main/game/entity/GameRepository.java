9db99db0fd9e0a5fb8545d958228c595fedc60cd
la1av1a <zldqkrktqk@gmail.com>
2022-07-12 01:35:38 +0900

    Style: 쿼리 수정 (count -> exists)

--- src/main/java/com/truetree/app/domain/main/game/entity/GameRepository.java
+++ src/main/java/com/truetree/app/domain/main/game/entity/GameRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface GameRepository extends JpaRepository<Game,Long> {
 
-    @Query("SELECT COUNT (g.id) >0 " +
-            "FROM Game g " +
-            "WHERE g.member.id = :memberId")
-    boolean existByMemberId(Long memberId);
+    boolean existsByMemberId(Long memberId);
 
 }
