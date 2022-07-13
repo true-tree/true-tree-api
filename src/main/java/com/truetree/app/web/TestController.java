82d61fcc18c0779f1dfdf87ce99af0978f82d3b9
pursue503 <pursue503@naver.com>
2022-07-14 00:03:35 +0900

    Add test API

--- src/main/java/com/truetree/app/web/TestController.java
+++ src/main/java/com/truetree/app/web/TestController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -28,6 +31,20 @@ public class TestController {
     @GetMapping("/get/coin/{count}")
     public List<BitCoinResponseDTO> getCoinData(@PathVariable("count") Integer range) {
         return bitCoinService.getBitCoinData(range);
+    }
+
+    @GetMapping("/cpu/test")
+    public List<Integer> list() {
+        List<Integer> list = new ArrayList<>();
+        int size = 100000;
+        Random random = new Random();
+        for (int i=0; i<size; i++) {
+            list.add(i);
+        }
+        int filterInt = random.nextInt(size);
+        return list.stream()
+                .filter(i -> filterInt == i)
+                .collect(Collectors.toList());
     }
 
 }
