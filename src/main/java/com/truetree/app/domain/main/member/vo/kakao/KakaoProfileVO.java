1dc3bbac77347915502bda3c89276877355f865f
pursue503 <pursue503@naver.com>
2022-07-23 13:09:27 +0900

    Fix : 카카오 로그인시 권한이 중복으로 들어가는 오류 해결

--- src/main/java/com/truetree/app/domain/main/member/vo/kakao/KakaoProfileVO.java
+++ src/main/java/com/truetree/app/domain/main/member/vo/kakao/KakaoProfileVO.java
@@ -1,9 +1,14 @@
 package com.truetree.app.domain.main.member.vo.kakao;
 
+import com.truetree.app.domain.main.member.entity.MemberAuthority;
+import com.truetree.app.domain.main.member.entity.enums.Authority;
 import com.truetree.app.domain.main.member.entity.enums.SocialType;
 import com.truetree.app.domain.main.member.entity.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -13,13 +18,18 @@ public class KakaoProfileVO {
 
     public Member toEntity() {
         String time = String.valueOf(System.currentTimeMillis()).substring(8);
-        return Member.builder()
+        Member member = Member.builder()
                 .snsId(String.valueOf(id))
                 .nickName(time + "new")
 //                .nickName("random")
+                .memberAuthorityList(new ArrayList<>())
                 .socialType(SocialType.KAKAO)
                 .money(1000_0000)
                 .build();
+
+        // 권한 초기화
+        member.initMemberAuthority();
+        return member;
     }
 
 }
